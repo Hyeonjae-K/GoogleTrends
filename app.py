e5f16831dd695972ba6f4bb96100adc3329bee06
unknown <rnjsguswo012@hufs.ac.kr>
2021-03-03 15:08:28 +0900

    Feat write data in text file

--- app.py
+++ app.py
@@ -20,11 +20,23 @@ def crawl_google():
     driver = webdriver.Chrome()
     # Google Trends url 접속
     driver.get("https://trends.google.com/trends/?geo=KR")
+    # 페이지를 모두 로드하기 위해 기다림
+    time.sleep(1)
 
     # div태그에 list-item-title클래스를 갖는 모든 태그를 titles에 list형태로 저장
     titles = driver.find_elements_by_css_selector("div.list-item-title")
     # div태그의 list-item-container클래스를 갖는 모든 태그를 urls에 list형태로 저장
     urls = driver.find_elements_by_css_selector("a.list-item-container")
+    # 크롤링한 데이터를 txt파일로 저장할 경로 지정
+    # 날짜가 지나면 자동으로 다음 날짜의 txt파일을 생성하고 데이터 저장
+    save_path = "./data/{date}.txt".format(date=global_date)
+
+    with open(save_path, "a", encoding="UTF-8") as f:
+        # 시간을 기록
+        f.write(global_hour + "시\n")
+        # 검색어와 url을 기록
+        for rank in range(10):
+            f.write(titles[rank].text + " " + urls[rank].get_attribute("href") + "\n")
 
     # Chrome driver 종료
     driver.close()
@@ -40,5 +52,6 @@ def index():
     return render_template("index.html")
 
 
+# Flask 실행
 if __name__ == '__main__':
     app.run()
